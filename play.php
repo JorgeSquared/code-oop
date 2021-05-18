d85a02eab6a42a4ae78638150e5374c3498214b7
Jiri Jahn <jiri.jahn@smsfinance.cz>
2021-05-18 16:03:16 +0200

    Mutliple objects interacting through their own methods (DOCTRINE)

--- play.php
+++ play.php
@@ -40,6 +40,11 @@ class Ship
 			);
 		}
 	 }
+
+	 public function doesGivenShipHaveMoreStrength($givenShip)
+	 {
+	 	return $givenShip->strength > $this->strength;
+	 }
 }
 
 function printShipSummary($someShip)
@@ -58,6 +63,7 @@ function printShipSummary($someShip)
 $myShip = new Ship();
 $myShip->name = 'Jedi Starship';
 $myShip->weaponPower = 10;
+$myShip->strength = 100;
 
 $otherShip = new Ship();
 $otherShip->name = 'Imperial Shadow';
@@ -67,3 +73,10 @@ $otherShip->strength = 50;
 printShipSummary($myShip);
 echo '<hr/<';
 printShipSummary($otherShip);
+echo '<hr/>';
+
+if ($myShip->doesGivenShipHaveMoreStrength($otherShip)) {
+	echo $otherShip->name.' has more strength';
+} else {
+	echo $myShip->name.' has more strength';
+}
