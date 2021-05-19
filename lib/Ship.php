199ecd130f96e465423d166554402c1f3e91fc5a
Jiri Jahn <jiri.jahn@smsfinance.cz>
2021-05-19 08:52:31 +0200

    Adding multiple ships, refactoring

--- lib/Ship.php
+++ lib/Ship.php
@@ -3,13 +3,53 @@
 
 class Ship
 {
-	public $name;
+	private $name;
 
-	public $weaponPower = 0;
+	private $weaponPower = 0;
 
-	public $jediFactor = 0;
+	private $jediFactor = 0;
 
-	public $strength = 0;
+	private $strength = 0;
+
+	/**
+	 * @return int
+	 */
+	public function getWeaponPower()
+	{
+		return $this->weaponPower;
+	}
+
+	/**
+	 * @param int $weaponPower
+	 */
+	public function setWeaponPower($weaponPower)
+	{
+		$this->weaponPower = $weaponPower;
+	}
+
+	/**
+	 * @return int
+	 */
+	public function getJediFactor()
+	{
+		return $this->jediFactor;
+	}
+
+	/**
+	 * @param int $jediFactor
+	 */
+	public function setJediFactor($jediFactor)
+	{
+		$this->jediFactor = $jediFactor;
+	}
+
+	/**
+	 * @param mixed $name
+	 */
+	public function setName($name)
+	{
+		$this->name = $name;
+	}
 
 	public function sayHello()
 	{
@@ -45,5 +85,19 @@ class Ship
 	public function doesGivenShipHaveMoreStrength($givenShip)
 	{
 		return $givenShip->strength > $this->strength;
+	}
+
+	public function setStrength($strength)
+	{
+		if (!is_numeric($strength)) {
+			throw new Exception('Invalid strength passed');
+		}
+
+		$this->strength = $strength;
+	}
+
+	public function getStrength()
+	{
+		return $this->strength;
 	}
 }
